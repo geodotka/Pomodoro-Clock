d003baf30497020704febca7ca885bc0ec73e186
geodotka <dorota.kowalik@21s.pl>
2019-08-25 16:12:46 +0200

    Refactor buttons listeners definition

--- main.js
+++ main.js
@@ -1,41 +1,52 @@
 function main() {
     const timer = new Timer();
-    // ----------------- decrease timer by 1 minute
-    const $minusButtons = document.getElementsByClassName('minus');
-    for (let i = 0; i < $minusButtons.length; i++) {
-        $minusButtons[i].addEventListener('click', function () {
-            const $item = document.getElementById(this.id);
-            const $sibling = $item.nextSibling.nextSibling; // DOMElement
-            timer.decreaseElementNumber($sibling);
+
+    function setupSessionButtons() {
+
+        const $sessionMinus = document.querySelector('#sessionMinus');
+        $sessionMinus.addEventListener('click', () => {
+            const $item = document.querySelector('#sessionTime');
+            timer.decreaseElementNumber($item);
+        });
+
+        const $breakMinus = document.querySelector('#breakMinus');
+        $breakMinus.addEventListener('click', () => {
+            const $item = document.querySelector('#breakTime');
+            timer.decreaseElementNumber($item);
         });
     }
 
+    function setupBreakButtons() {
 
-    // ----------------- increase timer by 1 minute
-    const $plusButtons = document.getElementsByClassName('plus');
-    for (i = 0; i < $plusButtons.length; i++) {
-        $plusButtons[i].addEventListener('click', function () {
-            const $item = document.getElementById(this.id);
-            const $sibling = $item.previousSibling.previousSibling; // DOMElement
-            timer.increaseElementNumber($sibling);
+        const $sessionPlus = document.querySelector('#sessionPlus');
+        $sessionPlus.addEventListener('click', () => {
+            const $item = document.querySelector('#sessionTime');
+            timer.increaseElementNumber($item);
+        });
+
+        const $breakPlus = document.querySelector('#breakPlus');
+        $breakPlus.addEventListener('click', () => {
+            const $item = document.querySelector('#breakTime');
+            timer.increaseElementNumber($item);
         });
     }
 
+    function setupClockButton() {
+        const $clock = document.querySelector('#wholeClock');
+        $clock.addEventListener('click', () => {
+            const $breakTime = document.getElementById('breakTime');
+            const $sessionTime = document.getElementById('sessionTime');
 
-    // -------------- timer countdown
-    // 1 minuta = 60 000 milisekund
-    // 1 sekunda = 1000 milisekund
-    // const intervalID = window.setInterval(function, 1000)
-    // var intervalID = window.setInterval(function, 1000)
-    document.getElementById('wholeClock').addEventListener('click', () => {
-        const $breakTime = document.getElementById('breakTime');
-        const $sessionTime = document.getElementById('sessionTime');
+            const $ele = document.getElementById('ele');
+            $ele.textContent = 'session';
 
-        const $ele = document.getElementById('ele');
-        $ele.textContent = 'session';
+            timer.startTimer($breakTime, $sessionTime);
+        });
+    }
 
-        timer.startTimer($breakTime, $sessionTime);
-    });
+    setupSessionButtons();
+    setupBreakButtons();
+    setupClockButton();
 }
 
 
