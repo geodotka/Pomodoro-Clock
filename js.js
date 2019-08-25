41c63698738e1abc649039140132dfc1ab8d8bff
geodotka <dorota.kowalik@21s.pl>
2019-08-25 12:27:39 +0200

    Change var for const or let

--- js.js
+++ js.js
@@ -1,11 +1,11 @@
 // ----------------- decrease timer by 1 minute
-var minusBttns = document.getElementsByClassName('minus');
+const minusBttns = document.getElementsByClassName('minus');
 for (i = 0; i < minusBttns.length; i++) {
     minusBttns[i].addEventListener('click', minusFunc);
 }
 
 function minusFunc() {
-    var currentTime = Number(document.getElementById(this.id).nextSibling.nextSibling.innerText);
+    let currentTime = Number(document.getElementById(this.id).nextSibling.nextSibling.innerText);
     if (currentTime !== 0) {
         currentTime -= 1;
     } else {
@@ -15,13 +15,13 @@ function minusFunc() {
 }
 
 // ----------------- increase timer by 1 minute
-var plusBttns = document.getElementsByClassName('plus');
+const plusBttns = document.getElementsByClassName('plus');
 for (i = 0; i < plusBttns.length; i++) {
     plusBttns[i].addEventListener('click', plusFunc);
 }
 
 function plusFunc() {
-    var currentTime = Number(document.getElementById(this.id).previousSibling.previousSibling.innerText);
+    let currentTime = Number(document.getElementById(this.id).previousSibling.previousSibling.innerText);
     if (currentTime < 60) {
         currentTime += 1;
     } else {
@@ -34,24 +34,24 @@ function plusFunc() {
 // -------------- timer countdown
 // 1 minuta = 60 000 milisekund
 // 1 sekunda = 1000 milisekund
-//var intervalID = window.setInterval(function, 1000)
-var clockCount = document.getElementById('wholeClock')
+//const intervalID = window.setInterval(function, 1000)
+const clockCount = document.getElementById('wholeClock')
     .addEventListener('click', timerOn);
 
 function timeCounter(arg1, arg2) {
-    var sessionTime = arg1;
-    var breakTime = arg2;
-    var interval = setInterval(function () {
+    const sessionTime = arg1;
+    const breakTime = arg2;
+    const interval = setInterval(function () {
         arg1 -= 1000;
-        var getDate = new Date();
+        const getDate = new Date();
         getDate.setTime(arg1);
-        var timer = document.getElementById('timer');
+        const timer = document.getElementById('timer');
         timer.textContent = ('0' + getDate.getMinutes()).slice(-2) + ':' + ('0' + getDate.getSeconds()).slice(-2);
 
         if (arg1 === 0) {
-            var snd = new Audio('FreesoundOrgSynthesizedHornByDarkadders.mp3');
+            const snd = new Audio('FreesoundOrgSynthesizedHornByDarkadders.mp3');
             snd.play();
-            var whatIsCounted = document.getElementById('ele');
+            const whatIsCounted = document.getElementById('ele');
             whatIsCounted.textContent = whatIsCounted.textContent == 'session' ? whatIsCounted.textContent = 'break' : whatIsCounted.textContent = 'session';
             clearInterval(interval);
             timeCounter(breakTime, sessionTime);
@@ -59,7 +59,7 @@ function timeCounter(arg1, arg2) {
     }, 1000);
 
     // ------ reset button
-    var resetBttn = document.getElementById('resetBttn')
+    const resetBttn = document.getElementById('resetBttn')
         .addEventListener('click', resetTimer);
 
     function resetTimer() {
@@ -70,8 +70,8 @@ function timeCounter(arg1, arg2) {
 }
 
 function timerOn() {
-    var userBreakTime = Number(document.getElementById('breakTime').innerText) * 60000;
-    var userSessionTime = Number(document.getElementById('sessionTime').innerText) * 60000;
+    const userBreakTime = Number(document.getElementById('breakTime').innerText) * 60000;
+    const userSessionTime = Number(document.getElementById('sessionTime').innerText) * 60000;
     document.getElementById('ele').textContent = 'session';
     timeCounter(userSessionTime, userBreakTime);
 }
