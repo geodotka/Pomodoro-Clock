819cb62d7e20f3f9f69d704dc28c89a8d5d7612a
geodotka <dorota.kowalik@21s.pl>
2019-08-25 12:50:43 +0200

    Define main function

--- js.js
+++ js.js
@@ -1,17 +1,25 @@
-// ----------------- decrease timer by 1 minute
-const $minusButtons = document.getElementsByClassName('minus');
-for (i = 0; i < $minusButtons.length; i++) {
-    $minusButtons[i].addEventListener('click', minusFunc);
+function main() {
+    // ----------------- decrease timer by 1 minute
+    const $minusButtons = document.getElementsByClassName('minus');
+    for (let i = 0; i < $minusButtons.length; i++) {
+        $minusButtons[i].addEventListener('click', minusFunc);
+    }
+
+
+    // ----------------- increase timer by 1 minute
+    const $plusButtons = document.getElementsByClassName('plus');
+    for (i = 0; i < $plusButtons.length; i++) {
+        $plusButtons[i].addEventListener('click', plusFunc);
+    }
+
+
+    // -------------- timer countdown
+    // 1 minuta = 60 000 milisekund
+    // 1 sekunda = 1000 milisekund
+    // const intervalID = window.setInterval(function, 1000)
+    // var intervalID = window.setInterval(function, 1000)
+    document.getElementById('wholeClock').addEventListener('click', timerOn);
 }
 
 
-// ----------------- increase timer by 1 minute
-const $plusButtons = document.getElementsByClassName('plus');
-for (i = 0; i < $plusButtons.length; i++) {
-    $plusButtons[i].addEventListener('click', plusFunc);
-}
-
-
-// -------------- timer countdown
-// 1 minuta = 60 000 milisekund
-// 1 sekunda = 1000 milisekund
+main();
