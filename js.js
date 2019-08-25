be6887552bb7c7231508d882dfedae291452c609
geodotka <dorota.kowalik@21s.pl>
2019-08-25 12:30:27 +0200

    Divide code for definition and running

--- js.js
+++ js.js
@@ -4,30 +4,11 @@ for (i = 0; i < minusBttns.length; i++) {
     minusBttns[i].addEventListener('click', minusFunc);
 }
 
-function minusFunc() {
-    let currentTime = Number(document.getElementById(this.id).nextSibling.nextSibling.innerText);
-    if (currentTime !== 0) {
-        currentTime -= 1;
-    } else {
-        currentTime = 0;
-    }
-    document.getElementById(document.getElementById(this.id).nextSibling.nextSibling.id).textContent = currentTime;
-}
 
 // ----------------- increase timer by 1 minute
 const plusBttns = document.getElementsByClassName('plus');
 for (i = 0; i < plusBttns.length; i++) {
     plusBttns[i].addEventListener('click', plusFunc);
-}
-
-function plusFunc() {
-    let currentTime = Number(document.getElementById(this.id).previousSibling.previousSibling.innerText);
-    if (currentTime < 60) {
-        currentTime += 1;
-    } else {
-        currentTime = 60;
-    }
-    document.getElementById(document.getElementById(this.id).previousSibling.previousSibling.id).textContent = currentTime;
 }
 
 
@@ -37,44 +18,3 @@ function plusFunc() {
 //const intervalID = window.setInterval(function, 1000)
 const clockCount = document.getElementById('wholeClock')
     .addEventListener('click', timerOn);
-
-function timeCounter(arg1, arg2) {
-    const sessionTime = arg1;
-    const breakTime = arg2;
-    const interval = setInterval(function () {
-        arg1 -= 1000;
-        const getDate = new Date();
-        getDate.setTime(arg1);
-        const timer = document.getElementById('timer');
-        timer.textContent = ('0' + getDate.getMinutes()).slice(-2) + ':' + ('0' + getDate.getSeconds()).slice(-2);
-
-        if (arg1 === 0) {
-            const snd = new Audio('FreesoundOrgSynthesizedHornByDarkadders.mp3');
-            snd.play();
-            const whatIsCounted = document.getElementById('ele');
-            whatIsCounted.textContent = whatIsCounted.textContent == 'session' ? whatIsCounted.textContent = 'break' : whatIsCounted.textContent = 'session';
-            clearInterval(interval);
-            timeCounter(breakTime, sessionTime);
-        }
-    }, 1000);
-
-    // ------ reset button
-    const resetBttn = document.getElementById('resetBttn')
-        .addEventListener('click', resetTimer);
-
-    function resetTimer() {
-        window.clearInterval(interval);
-        document.getElementById('ele').textContent = 'click';
-        document.getElementById('timer').textContent = 'to start';
-    }
-}
-
-function timerOn() {
-    const userBreakTime = Number(document.getElementById('breakTime').innerText) * 60000;
-    const userSessionTime = Number(document.getElementById('sessionTime').innerText) * 60000;
-    document.getElementById('ele').textContent = 'session';
-    timeCounter(userSessionTime, userBreakTime);
-}
-
-
-
